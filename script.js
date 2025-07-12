078e4452e90d0e7ab53f881a2708c1607f3969f3
premkumar0410 <rprem1042004@gmail.com>
2025-07-12 14:07:22 +0530

    fetching data from website using api

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const CityInput = document.querySelector(".city-input");
 const searchBtn = document.querySelector(".search-btn");
 
+
+const apiKey ="7af3665977bf208d9c27ad860a6bdfe8";
 searchBtn.addEventListener('click',()=>{
     if(CityInput.value.trim() != ""){
-        console.log(CityInput.value);
+       updateWeatherInfo(CityInput.value);
         CityInput.value ="";  
     }
 })
 
 CityInput.addEventListener('keydown',()=>{
     if(event.key == "Enter" && CityInput.value.trim() != ""){
-        console.log(CityInput.value);
+     updateWeatherInfo(CityInput.value);
         CityInput.value =""; 
     }
 })
+
+async function getFetchData(endpoint,city){
+    const apiurl =`https://api.openweathermap.org/data/2.5/${endpoint}?q=${city}&appid=${apiKey}&units=metric`;
+
+    const respone = await fetch(apiurl);
+    return respone.json()
+}
+
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData("weather", city);
+    console.log(weatherData);
+}
